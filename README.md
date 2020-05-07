c1c3dce88d7f3582358490e5bcc0ce04c16741a7
Christian Sakai <christianmsakai@gmail.com>
2020-05-07 02:08:12 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Black Hat Go
+
+From the book [Black Hat Go](https://www.amazon.com/Black-Hat-Go-Programming-Pentesters-ebook/dp/B073NPY29N/ref=sr_1_1)
---
+++ tcpproxy.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "net"
+  "log"
+  "io"
+  "fmt"
+)
+
+const dstPort = 81
+const srcPort = 80
+
+func handle(src net.Conn) {
+  dst, err := net.Dial("tcp", fmt.Sprintf(":%d", dstPort))
+  if err != nil {
+    log.Fatalln("Unable to connect to our unreachable host")
+  }
+
+  defer dst.Close()
+
+  go func() {
+    if _, err := io.Copy(dst, src); err != nil {
+      log.Fatalln(err)
+    }
+  }()
+
+  // Copy dst's output back to src
+  if _, err := io.Copy(src, dst); err != nil {
+    log.Fatalln(err)
+  }
+}
+
+func main() {
+  listener, err := net.Listen("tcp", fmt.Sprintf(":%d", srcPort))
+  if err != nil {
+    log.Fatalln("Unable to bind to port")
+  }
+
+  log.Println(fmt.Sprintf("Listening on 0.0.0.0:%d", srcPort))
+
+  for {
+    conn, err := listener.Accept()
+    if err != nil {
+      log.Fatalln("Unable to accept connection")
+    }
+
+    go handle(conn)
+  }
+}
